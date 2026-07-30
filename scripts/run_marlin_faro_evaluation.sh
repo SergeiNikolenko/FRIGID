56c7d62a216c53a2b81c17bedd0b11167c462ad7
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-07-30 17:00:41 +0300

    Expose evaluation conditioning dropout

--- scripts/run_marlin_faro_evaluation.sh
+++ scripts/run_marlin_faro_evaluation.sh
@@ -48,7 +48,7 @@ python -X faulthandler scripts/evaluate_marlin_nplib1.py \
   --output-dir "$OUTPUT_ROOT" \
   --candidates "${MARLIN_EVAL_CANDIDATES:-16}" \
   --max-spectra "${MARLIN_EVAL_MAX_SPECTRA:-4}" \
-  --diversity-dropout 0.3 \
+  --diversity-dropout "${MARLIN_EVAL_DIVERSITY_DROPOUT:-0.3}" \
   --temperature 1.0 \
   --generation-mode "${MARLIN_EVAL_GENERATION_MODE:-block}" \
   --ppm-tolerance 10.0 \
